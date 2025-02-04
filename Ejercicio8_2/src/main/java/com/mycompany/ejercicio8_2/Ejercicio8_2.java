871cb36fa12fd5d29d07ce3b025dee25458b6849
SoraHrS <soherreras@unal.edu.co>
2025-02-03 22:16:34 -0500

    Ejercicio 8.2 y 8.3 realizados

---
+++ Ejercicio8_2/src/main/java/com/mycompany/ejercicio8_2/Ejercicio8_2.java
@@ -0,0 +1,8 @@
+package com.mycompany.ejercicio8_2;
+
+public class Ejercicio8_2 {
+    public static void main(String[] args) {
+       VentanaPrincipal miVentanaPrincipal = new VentanaPrincipal();
+       miVentanaPrincipal.setVisible(true); 
+    }
+}
---
+++ Ejercicio8_3/src/main/java/com/mycompany/ejercicio8_3/Ejercicio8_3.java
@@ -0,0 +1,11 @@
+package com.mycompany.ejercicio8_3;
+
+public class Ejercicio8_3 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal;
+        miVentanaPrincipal= new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+        miVentanaPrincipal.setResizable(false);
+    }
+}
